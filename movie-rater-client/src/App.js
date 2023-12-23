2957f46b711749a90396bf23c0c28105a96aa6ae
achyvikov <anton.chyvikov@cengage.com>
2023-12-23 13:00:15 +0100

    implement basic state and the mocked list of movies

--- movie-rater-client/src/App.js
+++ movie-rater-client/src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  const [movies, setMovies] = useState([{ id: 1, title: 'Movie 1' }, { id: 2, title: 'Movie 2' }]);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-        <div className="">Movie List</div>
+        <div className="">
+          <h2>Movie List</h2>
+          <ul>
+            {movies.map(movie => {
+              return (
+                <li key={movie.id}>
+                  {movie.title}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
         <div className="">Movie Detail</div>
       </div>
     </div>
