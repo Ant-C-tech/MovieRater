b5522a508a7663ec75ec26d9e6f17eeed0eb9f84
achyvikov <anton.chyvikov@cengage.com>
2023-12-28 18:51:35 +0100

    implement api for getMovies and rateMovie

--- movie-rater-client/src/components/MovieDetails/index.jsx
+++ movie-rater-client/src/components/MovieDetails/index.jsx
@@ -7,67 +7,68 @@ import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons'
 import { faStar as lightStar } from '@fortawesome/free-regular-svg-icons'
 import { Button } from "react-bulma-components";
 
-export const MovieDetails = ({ selectedMovie }) => {
+import { getMovies, rateMovie } from "../../api/";
+
+export const MovieDetails = ({ selectedMovieId, movies, setMovies }) => {
   const [temporaryUserRating, setTemporaryUserRating] = useState(0);
   const [userRating, setUserRating] = useState(0);
 
   useEffect(() => {
     setUserRating(0)
     setTemporaryUserRating(0)
-  }, [selectedMovie])
+  }, [selectedMovieId])
 
-  const rateMovie = () => {
-    fetch(`http://127.0.0.1:8000/api/movies/${selectedMovie.id}/rate_movie/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Token 94df7a83ced3f398b7622adeac6b4f44320e3ae8'
-      },
-      body: JSON.stringify({ stars: userRating + 1 })
-    }).then(resp => resp.json())
-      .then(resp => console.log(resp))
-      .catch(error => console.log(error))
-  }
+  useEffect(() => {
+    console.log('movies', movies);
+  }, [movies])
 
-
-  if (selectedMovie) {
+  if (selectedMovieId) {
     return (
       <div className="movie-details">
         <h2 className="movie-details-title">Movie Details</h2>
-        {selectedMovie &&
-          <div className='movie-details-content'>
-            <p className='movie-details-content-description'>
-              {selectedMovie.description}
-            </p>
-            <p className='movie-details-content-stars'>
-              {Array.from({ length: 10 }).map((_, index) => {
-                return (
-                  <FontAwesomeIcon key={index} icon={selectedMovie.average_rating >= index ? solidStar : lightStar} />
-                )
-              })}
-            </p>
-            <p className='movie-details-content-ratings'>
-              <b>Number of ratings: {selectedMovie && selectedMovie.number_of_ratings}</b>
-            </p>
-          </div>}
+        <div className='movie-details-content'>
+          <p className='movie-details-content-description'>
+            {movies.find(movie => movie.id === selectedMovieId).description}
+          </p>
+          <p className='movie-details-content-stars'>
+            {Array.from({ length: 10 }).map((_, index) => {
+              return (
+                <FontAwesomeIcon key={index} icon={
+                  movies.find(movie => movie.id === selectedMovieId).average_rating >= index + 1 ? solidStar
+                    : lightStar} />
+              )
+            })}
+          </p>
+          <p className='movie-details-content-ratings'>
+            <b>Number of ratings: {movies.find(movie => movie.id === selectedMovieId).number_of_ratings}</b>
+          </p>
+        </div>
         <div className='movie-details-user-rating-control'>
           <div className='movie-details-user-stars'>
             {Array.from({ length: 10 }).map((_, index) => {
               return (
                 <FontAwesomeIcon
                   className='user-rating-stars'
-                  key={index} icon={temporaryUserRating >= index ? solidStar : lightStar}
-                  onMouseEnter={() => setTemporaryUserRating(index)}
+                  key={index} icon={temporaryUserRating > index ? solidStar : lightStar}
+                  onMouseEnter={() => setTemporaryUserRating(index + 1)}
                   onMouseLeave={() => setTemporaryUserRating(userRating)}
                   onClick={() => {
-                    setTemporaryUserRating(index)
-                    setUserRating(index)
+                    setTemporaryUserRating(index + 1)
+                    setUserRating(index + 1)
                   }
                   } />
               )
             })}
           </div>
-          <Button className='rate-movie-button' onClick={() => rateMovie()}>
+          <Button className='rate-movie-button' onClick={
+            () => {
+              rateMovie(selectedMovieId, userRating).then(() => {
+                getMovies()
+                  .then(movies => setMovies(movies))
+                  .catch(error => console.log(error))
+              }).catch(error => console.log(error))
+            }
+          }>
             Rate the Movie
           </Button>
         </div>
