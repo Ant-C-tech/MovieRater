0ac7224c38e2df1317a420416ba921c8fbe9aa7b
achyvikov <anton.chyvikov@cengage.com>
2024-01-02 20:32:48 +0100

    implement pages of the app for routing

--- movie-rater-client/src/App.jsx
+++ movie-rater-client/src/App.jsx
@@ -2,79 +2,43 @@ import 'normalize.css';
 import 'bulma/css/bulma.min.css';
 import './App.css';
 
-import { useState, useEffect } from 'react';
+import { Main, Auth } from './pages/';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilm as solidFilm } from '@fortawesome/free-solid-svg-icons'
-import { faPlus as solidPlus } from '@fortawesome/free-solid-svg-icons'
-import { Button } from 'react-bulma-components';
+import { Box } from 'react-bulma-components';
 
-
-import { MovieList, MovieDetails, EditMovie, CreateMovie } from './components/';
-import { API } from './api';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+]);
 
 function App() {
-
-  const [movies, setMovies] = useState([]);
-  const [selectedMovieId, setSelectedMovieId] = useState(null);
-  const [editedMovieId, setEditedMovieId] = useState(null);
-  const [isCreatingMovie, setIsCreatingMovie] = useState(false);
-
-  const storeMovies = async () => {
-    const movies = await API.getMovies();
-    setMovies(movies);
-  }
-
-  useEffect(() => {
-    storeMovies();
-  }, []);
-
   return (
     <div className="app">
       <header className="app-header">
         <FontAwesomeIcon className='app-title-logo' icon={solidFilm} />
         <h1 className='app-title'> Movie Rater</h1>
-        <Button className='button-success' color="success" onClick={() => {
-          setSelectedMovieId(null);
-          setEditedMovieId(null);
-          setIsCreatingMovie(true);
-        }
-        }>
-          <FontAwesomeIcon className='button-icon' icon={solidPlus} />
-        </Button>
       </header>
-      <div className="layout">
-        <MovieList
-          movies={movies}
-          setMovies={setMovies}
-          selectedMovieId={selectedMovieId}
-          setSelectedMovieId={setSelectedMovieId}
-          setEditedMovieId={setEditedMovieId}
-          setIsCreatingMovie={setIsCreatingMovie}
-        />
-
-        {editedMovieId ?
-          <EditMovie
-            editedMovieId={editedMovieId}
-            setEditedMovieId={setEditedMovieId}
-            movies={movies}
-            setMovies={setMovies}
-            setIsCreatingMovie={setIsCreatingMovie}
-          /> :
-          isCreatingMovie ?
-            <CreateMovie
-              movies={movies}
-              setMovies={setMovies}
-              setIsCreatingMovie={setIsCreatingMovie}
-            />
-            :
-            <MovieDetails
-              selectedMovieId={selectedMovieId}
-              movies={movies}
-              setMovies={setMovies}
-              setIsCreatingMovie={setIsCreatingMovie}
-            />
-        }
-      </div>
+      <Box style={{
+        height: '83vh',
+        maxHeight: '83vh',
+        backgroundColor: 'inherit',
+        border: '1px solid #48c78e',
+        color: '#ccc',
+        textAlign: 'center'
+      }}>
+        <RouterProvider router={router} />
+      </Box>
     </div >
   );
 }
--- movie-rater-client/src/index.js
+++ movie-rater-client/src/index.js
@@ -1,26 +1,11 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import { Auth } from './components/Auth/index.jsx';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/auth",
-    element: <Auth />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
