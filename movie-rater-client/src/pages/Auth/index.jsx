e0bb9af11aa2848a8230267f999cee43e1e40e7c
achyvikov <anton.chyvikov@cengage.com>
2024-01-03 17:16:06 +0100

    imlement user login api

--- movie-rater-client/src/pages/Auth/index.jsx
+++ movie-rater-client/src/pages/Auth/index.jsx
@@ -3,43 +3,68 @@ import "./style.css";
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightToBracket as solidRightToBracket } from '@fortawesome/free-solid-svg-icons'
-import { Button, Form } from 'react-bulma-components';
+import { Button, Form, Box } from 'react-bulma-components';
+
+import { API } from '../../api/';
 
 const { Field, Label, Input, Control } = Form;
 
+const loginUser = async (username, password) => {
+  try {
+    const response = await API.loginUser({ username, password });
+    console.log('User logged in successfully. Token: ', response.token);
+  } catch (error) {
+    console.error('There was a problem with the login operation: ', error);
+  }
+};
+
 export const Auth = () => {
-  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   return (
     <div className='auth-wrapper'>
-      <h2 className="auth-title">Login</h2>
-      <Field>
-        <Label className='auth-label' size='medium'>
-          Name
-        </Label>
-        <Control>
-          <Input
-            placeholder='e.g. John Doe'
-            type="text"
-            onChange={(event) => setName(event.target.value)}
-          />
-        </Control>
-      </Field>
-      <Field>
-        <Label className='movie-form-label' size='medium'>
-          Password
-        </Label>
-        <Control>
-          <Input
-            placeholder='e.g. qwertyqwerty'
-            type="password"
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </Control>
-      </Field>
+      <h2 className="auth-form-title">Login</h2>
+      <Box style={{
+        width: '50%',
+        margin: '0 auto 20px',
+        backgroundColor: 'inherit',
+        color: '#ccc',
+        textAlign: 'center'
+      }}>
+        <Field>
+          <Label className='auth-form-label' size='medium'>
+            Name
+          </Label>
+          <p>150 characters or fewer. Letters, digits and @/./+/-/_ only.</p><br />
+          <Control>
+            <Input
+              placeholder='e.g. John Doe'
+              type="text"
+              value={username}
+              maxLength={150}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </Control>
+        </Field>
+        <Field>
+          <Label className='auth-form-label' size='medium'>
+            Password
+          </Label>
+          <Control>
+            <Input
+              placeholder='e.g. qwertyqwerty'
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </Control>
+        </Field>
+      </Box>
       <div className='auth-controls'>
-        <Button className='button-custom'>
+        <Button className='button-custom' onClick={
+          () => loginUser(username, password)
+        }>
           <FontAwesomeIcon className='button-icon' icon={solidRightToBracket} />
           Login
         </Button>
